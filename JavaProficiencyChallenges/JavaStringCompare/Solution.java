4831ad4e68e00d6a6f6dd7eb35599a747d44750b
Anthony Aoun <anthony.aoun1@hotmail.com>
2018-09-14 15:29:07 -0400

    more challenges

---
+++ JavaProficiencyChallenges/JavaStringCompare/Solution.java
@@ -0,0 +1,30 @@
+
+// https://www.hackerrank.com/challenges/java-string-compare/problem
+
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        SortedSet<String> set = new TreeSet<String>();
+
+        for (int i = 0; i < s.length() - k; i++)
+            set.add(s.substring(i, i + k));
+        smallest = set.first();
+        largest = set.last();
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
